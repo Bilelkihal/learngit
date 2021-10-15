2ff3f26fc5c35b794cf3aa665fd1171e4f3aedf0
bilelkihal <kb_kihal@esi.dz>
2021-10-15 22:51:03 +0100

    add dev screen

---
+++ lib/screens/DevScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DevScreen extends StatelessWidget {
+  const DevScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
